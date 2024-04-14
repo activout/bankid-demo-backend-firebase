ef1fd77120090578322a8e458baf4d4eb3b6c43c
David Eriksson <david@activout.se>
2024-04-14 17:06:26 +0200

    Working Firebase functions backend

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,9 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
+const BankIdClientV6 = require("bankid").BankIdClientV6;
+
+// const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
@@ -17,3 +19,57 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const client = new BankIdClientV6({
+  production: false,
+  qrEnabled: false,
+});
+
+const {onCall} = require("firebase-functions/v2/https");
+
+exports.auth = onCall(async (request) => {
+  const data = request.data;
+  logger.info("auth request", data);
+  const result = await client.authenticate({
+    endUserIp: request.rawRequest.ip,
+  });
+  logger.info("auth result", result);
+  return {
+    autoStartToken: result.autoStartToken,
+    orderRef: result.orderRef,
+  };
+});
+
+exports.sign = onCall(async (request) => {
+  const data = request.data;
+  logger.info("sign request", data);
+  const result = await client.sign({
+    endUserIp: request.rawRequest.ip,
+    userVisibleData: "This is a demo from Activout AB.",
+  });
+  logger.info("sign result", result);
+  return {
+    autoStartToken: result.autoStartToken,
+    orderRef: result.orderRef,
+  };
+});
+exports.collect = onCall(async (request) => {
+  const data = request.data;
+  logger.debug("collect request", data);
+  const result = await client.collect({orderRef: data.orderRef});
+  if (result.status !== "pending") {
+    logger.info("collect result", result);
+  }
+  return JSON.stringify(result);
+});
+
+exports.cancel = onCall(async (request) => {
+  const data = request.data;
+  logger.debug("cancel request", data);
+  try {
+    await client.cancel({orderRef: data.orderRef});
+  } catch (e) {
+    console.error(e);
+  }
+  return {};
+});
